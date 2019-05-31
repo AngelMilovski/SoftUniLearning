0737079a8f424cad2c5862cc2033aeb5b08c48bd
AngelMilovski <angelmilovski8822@gmail.com>
2019-06-01 00:47:44 +0300

    Fixed Binary-Decoding/solution.js

--- 08.JSEssentials/04.StringsAndRegEx/02.Exercise/01_Binary-Decoding/solution.js
+++ 08.JSEssentials/04.StringsAndRegEx/02.Exercise/01_Binary-Decoding/solution.js
@@ -10,10 +10,9 @@ function solve () {
         .reduce((a, b) => a + b, 0);
     }
   
-    let test = input
+    let result = input
       .slice(weight, input.length - weight)
-      .match(/.{1,8}/g);
-    let result = test
+      .match(/.{1,8}/g)
       .map(segment => String.fromCharCode(parseInt(segment, 2)))
       .filter(x => /[A-Za-z\s]/.test(x))
       .join('');
